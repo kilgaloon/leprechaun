c6e088db1d262390299a3dfa64d86cac29d3992e
kilgaloon <radju22@gmail.com>
2018-10-06 18:16:45 +0200

    Adding tests

--- client/main_test.go
+++ client/main_test.go
@@ -1,10 +1,9 @@
 package client
 
 import (
-	"io/ioutil"
-	"os"
-	"strconv"
 	"testing"
+
+	"github.com/kilgaloon/leprechaun/event"
 
 	"github.com/kilgaloon/leprechaun/config"
 )
@@ -17,42 +16,22 @@ var (
 )
 
 func TestStart(t *testing.T) {
-	// remove hanging .lock file
-	os.Remove(fakeClient.Agent.GetConfig().GetLockFile())
-	// SetPID of client
-	fakeClient.SetPID()
-	// build queue
-
-	// Fail because clien't isn't working anything on start
-	if fakeClient.isWorking() {
-		t.Error("Client should not be working anything here")
-	}
-
-	b, err := ioutil.ReadFile(fakeClient.Agent.GetConfig().GetPIDFile())
-	if err != nil {
-		t.Errorf("%s", err)
-	}
-
-	str := string(b)
-	pid, err := strconv.Atoi(str)
-	if err != nil {
-		t.Error(err)
-	}
-
-	if fakeClient.GetPID() != pid {
-		t.Errorf("PID expected to be %d but got %d", pid, fakeClient.GetPID())
-	}
+	go fakeClient.Start()
+	t.Run("stop", func(t *testing.T) {
+		fakeClient.Stop()
+	})
 }
-func TestLock(t *testing.T) {
-	fakeClient.Lock()
 
+func TestLockUnlock(t *testing.T) {
+	fakeClient.Lock()
 	if !fakeClient.isWorking() {
 		t.Fail()
 	}
+	event.EventHandler.Dispatch("client:unlock")
+}
 
-	fakeClient.Unlock()
-
-	if fakeClient.isWorking() {
+func TestGetAgent(t *testing.T) {
+	if fakeClient.GetAgent() != fakeClient.Agent {
 		t.Fail()
 	}
 }
--- client/queue_test.go
+++ client/queue_test.go
@@ -4,35 +4,42 @@ import (
 	"testing"
 )
 
-func TestBuildQueue(t *testing.T) {
-	fakeClient.BuildQueue()
 
-	if len(fakeClient.Queue.Stack) != 1 {
-		t.Errorf("Queue stack length expected to be 1, got %d", len(fakeClient.Queue.Stack))
+var (
+	fk = New("test", cfgWrap.New("test", *path))
+)
+
+func TestBuildQueue(t *testing.T) {
+	fk.BuildQueue()
+
+	if len(fk.Queue.Stack) != 1 {
+		t.Errorf("Queue stack length expected to be 1, got %d", len(fk.Queue.Stack))
 	}
 
-	// reset queue to 0 to test AddToQueue
-	fakeClient.Queue.Stack = fakeClient.Queue.Stack[:0]
+	// reset queue to 0 to test AddToQueue	
+	q := &fk.Queue
+	q.Stack = q.Stack[:0]
 }
 
 func TestAddToQueue(t *testing.T) {
-	fakeClient.AddToQueue(&fakeClient.Queue.Stack, fakeClient.Agent.GetConfig().GetRecipesPath()+"/schedule.yml")
-	if len(fakeClient.Queue.Stack) != 1 {
+	fk.AddToQueue(&fk.Queue.Stack, fk.Agent.GetConfig().GetRecipesPath()+"/schedule.yml")
+	if len(fk.Queue.Stack) != 1 {
 		t.Errorf("Queue stack length expected to be 1, got %d", len(fakeClient.Queue.Stack))
 	}
 
-	fakeClient.AddToQueue(&fakeClient.Queue.Stack, fakeClient.Agent.GetConfig().GetRecipesPath()+"/hook.yml")
-	if len(fakeClient.Queue.Stack) != 1 {
+	fk.AddToQueue(&fk.Queue.Stack, fk.Agent.GetConfig().GetRecipesPath()+"/hook.yml")
+	if len(fk.Queue.Stack) != 1 {
 		t.Errorf("Queue stack length expected to be 0, got %d", len(fakeClient.Queue.Stack))
 	}
 }
 
-// func TestProcessQueue(t *testing.T) {
-// 	now := time.Now()
-// 	compare := time.Date(now.Year(), now.Month(), now.Day(), now.Hour(), now.Minute(), 0, 0, time.UTC)
+func TestProcessRecipe(t *testing.T) {
+	fakeClient.ProcessRecipe(&fakeClient.Queue.Stack[0])
+}
 
-// 	for index, r := range fakeClient.Queue.Stack {
-// 		recipe := &fakeClient.Queue.Stack[index]
-
-// 	}
-// }
+func TestClientInfo(t *testing.T) {
+	_, err := fakeClient.clientInfo()
+	if err != nil {
+		t.Fail()
+	}
+}
