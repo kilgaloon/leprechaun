7fc462568c33bb30008829ace87dcb56eb706f3c
kilgaloon <radju22@gmail.com>
2019-03-15 16:58:11 +0100

    Updating server tests and possible race condition

--- server/main_test.go
+++ server/main_test.go
@@ -107,7 +107,10 @@ func TestStartStop(t *testing.T) {
 		break
 	}
 
+	Agent.Lock()
 	fakeServer2.GetConfig().Domain = "https://localhost"
+	Agent.Unlock()
+
 	go fakeServer2.Start()
 
 }
@@ -127,7 +130,10 @@ func TestFindInPool(t *testing.T) {
 }
 
 func TestIsTLS(t *testing.T) {
+	Agent.Lock()
 	fakeServer2.GetConfig().Domain = "localhost"
+	Agent.Unlock()
+
 	if Agent.isTLS() {
 		t.Fail()
 	}
